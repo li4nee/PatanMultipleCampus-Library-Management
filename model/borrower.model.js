c32bff57b240ffebf39de87a80f0e596e1304afa
Li4nee <nishantpokharel9@gmail.com>
2024-03-05 18:53:32 +0545

    models for borrower, fine and return

---
+++ model/borrower.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const borrowerSchema = mongoose.Schema(
+  {
+    bookId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    issuedDate: {
+      type: String,
+      default:Date.now()
+    },
+    notReturned:{
+        type:Boolean,
+        default:true
+    }
+  },
+  { timestamps: true }
+);
+
+const Borrower = mongoose.model("Borrower", borrowerSchema);
+export default Book;
