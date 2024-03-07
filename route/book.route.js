ef5364162095e62ec9ee5b4a6b0156d13bbdfca8
Li4nee <nishantpokharel9@gmail.com>
2024-03-07 14:46:57 +0545

    fixing bugs in router

--- route/book.route.js
+++ route/book.route.js
@@ -1,8 +1,20 @@
 import Router from "express";
-import { addBook, showAllBook } from "../controller/book.controller.js";
+import {
+  addBook,
+  showAllBook,
+  borrowBook,
+  findBorrowTransaction,returnBook
+} from "../controller/book.controller.js";
+import { checkTokenMiddleware } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.get("/", showAllBook);
-router.post("/addBook", addBook);
-
+router.post("/addBook", checkTokenMiddleware, addBook);
+router.get("/borrow/:ISBN", checkTokenMiddleware, borrowBook);
+router.post(
+  "/findBorrowTransaction",
+  checkTokenMiddleware,
+  findBorrowTransaction
+);
+router.get("/return/:bookId",checkTokenMiddleware,returnBook)
 export default router;
--- route/user.route.js
+++ route/user.route.js
@@ -1,12 +1,12 @@
 import Router from "express";
-import { loginUser,logoutUser,showUserProfile,signupUser } from "../controller/user.controller.js";
+import { loginUser,logoutUser,seeTransactionDetail,showUserProfile,signupUser } from "../controller/user.controller.js";
 import { validateRegistration } from "../middleware/validator.middleware.js";
 import { checkTokenMiddleware } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.post("/login",loginUser);
 router.post("/signup",validateRegistration,signupUser);
-router.get("/logout",logoutUser);
-router.get("/profile/:_id",checkTokenMiddleware,showUserProfile);
-
+router.get("/logout",checkTokenMiddleware,logoutUser);
+router.get("/profile",checkTokenMiddleware,showUserProfile);
+router.get("/transactionDetail/:transactionToken",checkTokenMiddleware,seeTransactionDetail);
 export default router;
